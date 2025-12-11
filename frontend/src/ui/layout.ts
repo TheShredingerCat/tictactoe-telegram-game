d8e389116b8b1513e268f39ddc99673fa0d03f05
Ананастя <the.shredinger.cat@gmail.com>
2025-12-11 11:38:34 +0300

    продолжение правок отображения

--- frontend/src/ui/layout.ts
+++ frontend/src/ui/layout.ts
@@ -1,8 +1,6 @@
 const screenGame = document.getElementById("screen-game") as HTMLElement;
 const screenLose = document.getElementById("screen-lose") as HTMLElement;
-
 const promoModalEl = document.getElementById("promo-modal") as HTMLElement;
-
 const statusTextEl = document.getElementById("status-text") as HTMLElement;
 
 export const layout = {
@@ -10,17 +8,12 @@ export const layout = {
     screenGame.classList.remove("screen--hidden");
     screenLose.classList.add("screen--hidden");
     promoModalEl.classList.add("promo-modal--hidden");
-
-    screenGame.classList.add("screen--active");
-    screenLose.classList.remove("screen--active");
   },
 
   showLoseScreen() {
     screenGame.classList.add("screen--hidden");
     screenLose.classList.remove("screen--hidden");
     promoModalEl.classList.add("promo-modal--hidden");
-
-    screenLose.classList.add("screen--active");
   },
 
   showWinModal() {
